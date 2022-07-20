822b82be50d30b59e518c086d2b3b4b1c378ace5
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-07-19 23:32:41 -0300

    ii

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,46 +1,40 @@
-/* ************************************************************************************* */
-/*                                                                                       */
-/*                                           ::::::::   :::::::      ::::::::  ::::::::  */
-/* ft_memset.c                               :+:    :+:   :+:       :+:    :+::+:    :+: */
-/*                                           +:+    +:+   +:+       +:+       +:+        */
-/* By: Diogo-ss <diogo-ss@mail.com>          +#+    +:+   +#+  ++#+ +#++:++#+++#++:++#++ */
-/*                                           +#+    +#+   +#+              +#+       +#+ */
-/* Created: 2022/06/29 18:00:11 by Diogo-ss  #+#    #+#   #+#       #+#    #+##+#    #+# */
-/* Updated: 2022/06/29 18:36:17 by Diogo-ss  ########   #######      ########  ########  */
-/*                                                                                       */
-/* ************************************************************************************* */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                               ::::::::   :::::::           */
+/*   ft_memset.c                                 :+:    :+:   :+:             */
+/*                                               +:+    +:+   +:+             */
+/*   By: Diogo-ss <diogo-ss@mail.com>            +#+    +:+   +#+             */
+/*                                               +#+    +#+   +#+             */
+/*   Created: 2022/06/29 18:00:11 by Diogo-ss    #+#    #+#   #+#             */
+/*   Updated: 2022/06/29 18:36:17 by Diogo-ss    ########   #######           */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-    size_t i;
-    char val;
-    char *dest_l;
-    char *src_l;
+	size_t	i;
+	char	val;
+	char	*dest_l;
+	char	*src_l;
 
-    val = (char) c;
-    dest_l = (char *) dest;
-    src_l = (char *) src;
-
-    if (!(dest || src))
-        return NULL;
-
-    i = 0;
-    while (i < n) 
-    {
-        *dest_l = *src_l;
-
-        if (*dest_l == val)
-        {
-            return (void *)dest; 
-//             break;
-        }
-        
-        dest_l++;
-        src_l++;
-        i++;
-    }
-
-    return NULL;
+	val = (char) c;
+	dest_l = (char *) dest;
+	src_l = (char *) src;
+	if (!(dest || src))
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		*dest_l = *src_l;
+		if (*dest_l == val)
+		{
+			return ((void *)dest);
+		}
+		dest_l++;
+		src_l++;
+		i++;
+	}
+	return (NULL);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,35 +1,34 @@
-/* ************************************************************************************* */
-/*                                                                                       */
-/*                                           ::::::::   :::::::      ::::::::  ::::::::  */
-/* ft_memset.c                               :+:    :+:   :+:       :+:    :+::+:    :+: */
-/*                                           +:+    +:+   +:+       +:+       +:+        */
-/* By: Diogo-ss <diogo-ss@mail.com>          +#+    +:+   +#+  ++#+ +#++:++#+++#++:++#++ */
-/*                                           +#+    +#+   +#+              +#+       +#+ */
-/* Created: 2022/06/29 18:00:11 by Diogo-ss  #+#    #+#   #+#       #+#    #+##+#    #+# */
-/* Updated: 2022/06/29 18:36:17 by Diogo-ss  ########   #######      ########  ########  */
-/*                                                                                       */
-/* ************************************************************************************* */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                               ::::::::   :::::::           */
+/*   ft_memset.c                                 :+:    :+:   :+:             */
+/*                                               +:+    +:+   +:+             */
+/*   By: Diogo-ss <diogo-ss@mail.com>            +#+    +:+   +#+             */
+/*                                               +#+    +#+   +#+             */
+/*   Created: 2022/06/29 18:00:11 by Diogo-ss    #+#    #+#   #+#             */
+/*   Updated: 2022/06/29 18:36:17 by Diogo-ss    ########   #######           */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
-char    *ft_memmove(char *dest, char *src, size_t len)
+char	*ft_memmove(char *dest, char *src, size_t len)
 {
-    unsigned int count;
+	unsigned int	count;
 
-    count = 0;
-    if (dest < src)
+	count = 0;
+	if (dest < src)
 	{
-        while (count < len) 
-        {
-            dest[count] = src[count];
-            count++;
-        }
-        return dest;
-    }
-
-    while (len--) 
-    { 
-        dest[len] = src[len]; 
-    }
-    return dest;
+		while (count < len)
+		{
+			dest[count] = src[count];
+			count++;
+		}
+		return (dest);
+	}
+	while (len--)
+	{
+		dest[len] = src[len];
+	}
+	return (dest);
 }
